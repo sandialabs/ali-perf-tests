06c71ff43a48df58479083abd10ea211c23ff318
Irina Tezaur <ikalash@sandia.gov>
2019-10-05 23:03:49 -0600

    Setting up nightly performane tests.

--- cdash/process_results_ctest.sh
+++ cdash/process_results_ctest.sh
@@ -1,18 +1,18 @@
 
-grep "Test   #" nightly_log_blakeMiniEMPerfTests.txt >& results0
-grep "Test  #" nightly_log_blakeMiniEMPerfTests.txt >& results1
+grep "Test   #" nightly_log_blakeAliPerfTests.txt >& results0
+grep "Test  #" nightly_log_blakeAliPerfTests.txt >& results1
 cat results0 results1 >& results11
-grep "Test #" nightly_log_blakeMiniEMPerfTests.txt >& results0
+grep "Test #" nightly_log_blakeAliPerfTests.txt >& results0
 cat results11 results0 >& results1
 echo "" >> results1
-grep " tests failed" nightly_log_blakeMiniEMPerfTests.txt >& results2
+grep " tests failed" nightly_log_blakeAliPerfTests.txt >& results2
 cat results1 results2 >& results3
-grep "Total Test" nightly_log_blakeMiniEMPerfTests.txt >& results4
+grep "Total Test" nightly_log_blakeAliPerfTests.txt >& results4
 cat results3 results4 >& results5
 echo "" >> results5
-grep "(Failed)" nightly_log_blakeMiniEMPerfTests.txt >& results6
+grep "(Failed)" nightly_log_blakeAliPerfTests.txt >& results6
 cat results5 results6 >& results7
-grep "\*Timeout" nightly_log_blakeMiniEMPerfTests.txt >& results8
+grep "\*Timeout" nightly_log_blakeAliPerfTests.txt >& results8
 cat results7 results8 >& results
 echo "" >> results 
 echo "The Albany CDash site can be accessed here: https://my.cdash.org/index.php?project=Albany" >> results
--- cdash/send_email.sh
+++ cdash/send_email.sh
@@ -2,13 +2,13 @@
   
 #source $1 
 
-TTT=`grep "(Failed)" nightly_log_blakeMiniEMPerfTests.txt -c`
-TTTT=`grep "Not Run" nightly_log_blakeMiniEMPerfTests.txt -c`
-TTTTT=`grep "\*Timeout" nightly_log_blakeMiniEMPerfTests.txt -c`
-TT=`grep "...   Passed" nightly_log_blakeMiniEMPerfTests.txt -c`
+TTT=`grep "(Failed)" nightly_log_blakeAliPerfTests.txt -c`
+TTTT=`grep "Not Run" nightly_log_blakeAliPerfTests.txt -c`
+TTTTT=`grep "\*Timeout" nightly_log_blakeAliPerfTests.txt -c`
+TT=`grep "...   Passed" nightly_log_blakeAliPerfTests.txt -c`
 
 
-echo "Subject: MiniEM, blake: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" >& a
+echo "Subject: ALI performance tests, blake: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" >& a
 echo "" >& b
 cat a b >& c
 cat c results >& d

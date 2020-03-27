ed6e1cc100ebb83e00b8fe606c64f0d87780bd22
Irina K. Tezaur <ikalash@sandia.gov>
2020-03-27 09:39:13 -0700

    Disabling email from waterman perf nightly, now that it is showing up in the new cdash site.

--- cdash/waterman/process_results_ctest.sh
+++ cdash/waterman/process_results_ctest.sh
@@ -20,4 +20,4 @@ echo "" >> results
 echo "The Jupyter notebooks containing Waterman ALI performance data can be accessed here: https://ikalash.github.io/ali/index_waterman.html" >> results
 echo "" >> results
 rm results0 results1 results11 results2 results3 results4 results5 results6 results7 results8
-bash send_email.sh
+#bash send_email.sh

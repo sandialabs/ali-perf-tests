d599e2108a03341bec7d2afbd0757c055bf97d20
Jerry Watkins <watkins2@alumni.stanford.edu>
2023-02-14 09:54:56 -0700

    Update kokkos prof lib and expected responses.  - also small fix to blake beta tests

--- doc/submit_blake.sh
+++ doc/submit_blake.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -login
 
-#SBATCH --job-name=GISRun
-#SBATCH --output=GISRun.%j.out
-#SBATCH --error=GISRun.%j.err
+#SBATCH --job-name=ALIPT
+#SBATCH --output=ALIPT.%j.out
+#SBATCH --error=ALIPT.%j.err
 #SBATCH --nodes=8
 #SBATCH --ntasks=384
 #SBATCH --ntasks-per-node=48
@@ -14,8 +14,8 @@
 source ${HOME}/bin/blake_intel_modules.sh
 
 # Env variables
-#export KOKKOS_PROFILE_LIBRARY=/home/projects/albany/nightlyCDashAlbanyBlake/kokkos-tools/profiling/space-time-stack-mem-only/kp_space_time_stack.so
-unset KOKKOS_PROFILE_LIBRARY
+export KOKKOS_TOOLS_LIBS=/home/projects/albany/nightlyCDashAlbanyBlake/kokkos-tools/profiling/space-time-stack-mem-only/kp_space_time_stack.so
+#unset KOKKOS_TOOLS_LIBS
 
 # Run
 ctest -V

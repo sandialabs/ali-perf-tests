b5516361e1bc95decc5a4695c34dd1b671936bba
Jerry Watkins <watkins2@alumni.stanford.edu>
2024-11-05 18:26:00 -0700

    Add MALI cases and more updates to perf. tests  - reduce to one node on blake (and turn off cases that don't fit)  - fix optimization case  - fix fea cases  - add scaling to cop problems

--- doc/submit_blake.sh
+++ doc/submit_blake.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=ALIPT
 #SBATCH --output=ALIPT.%j.out
 #SBATCH --error=ALIPT.%j.err
-#SBATCH --nodes=2
-#SBATCH --ntasks=192
+#SBATCH --nodes=1
+#SBATCH --ntasks=96
 #SBATCH --ntasks-per-node=96
 #SBATCH --ntasks-per-socket=48
 #SBATCH --time=04:00:00
@@ -12,8 +12,8 @@
 ## SBATCH --exclude=node03,node06,node10,node27,node15
 
 # Load modules
-source blake_gcc_modules.sh
-#source blake_intel_modules.sh
+source ${HOME}/bin/blake_gcc_modules.sh
+#source ${HOME}/bin/blake_intel_modules.sh
 
 # Env variables
 export KOKKOS_TOOLS_LIBS=/home/projects/albany/nightlyCDashAlbanyBlake/kokkos-tools/profiling/space-time-stack-mem-only/kp_space_time_stack.so
